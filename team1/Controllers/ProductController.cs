72ac59eb3fc1624595816be2e2209234fbce79da
GidXn <153444661+GydiX@users.noreply.github.com>
2025-05-21 16:50:19 +0300

    Added Controllers
    Створено контролери -> ProductController, CategoryController
    Додано методи CRUD (без реалізації)

---
+++ team1/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace team1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] object productDto)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = 0 }, productDto);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] object productDto)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
