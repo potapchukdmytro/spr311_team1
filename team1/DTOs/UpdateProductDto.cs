9de75954a7a462471e72daad5eafe7e25ac18db0
GidXn <153444661+GydiX@users.noreply.github.com>
2025-05-21 16:46:55 +0300

    Added DTOs validation
    Додано FluentValidation та налаштовано валідацію для dtos

---
+++ team1/DTOs/UpdateProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace team1.DTOs
+{
+    public class UpdateProductDto
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+}
---
+++ team1/DTOs/Validators/UpdateCategoryDtoValidator.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+using team1.DTOs;
+
+public class UpdateCategoryDtoValidator : AbstractValidator<UpdateCategoryDto>
+{
+    public UpdateCategoryDtoValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required.");
+    }
+}
