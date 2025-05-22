814a031b8669cf3fbeeb1a41e51176d820c5a6a8
Dima0913 <153444026+Dima0913@users.noreply.github.com>
2025-05-22 16:58:10 +0300

    Add files via upload

---
+++ team1/team1/team1/CategoryRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using team1.Data;
+using team1.Models;
+
+namespace team1.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> GetByIdAsync(int id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public async Task AddAsync(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Category category)
+        {
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
